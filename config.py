efb865afc97b649ceb25db94629866bfb6143b49
ishaansawant0306 <24f2007256@ds.study.iitm.ac.in>
2026-07-11 13:02:54 +0530

    Change EMAIL and AIPIPE_TOKEN in config.py
    Updated EMAIL and AIPIPE_TOKEN with new values.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # The only two things you must fill in:
-EMAIL = "your_email@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "PASTE_YOUR_AIPIPE_TOKEN"
+EMAIL = "24f2007256@ds.study.iitm.ac.in"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjIwMDcyNTZAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4Mzc1NTE2NCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDM1OTk2NH0.unYMe9i202WO-xnDrjV93zTOFMhDXc8UPsa0zBLK6OA"
 
 # Fixed — do not change
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
